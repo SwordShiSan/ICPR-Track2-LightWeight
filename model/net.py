f9ef4e67cf0121797c954b3926fdab93d5b2f2e2
SwordShiSan <2486511577@qq.com>
2024-07-07 17:03:36 +0800

    lw4 1024

--- model/net.py
+++ model/net.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch
 from torchvision import models
-# from torchviz import make_dot,make_dot_from_trace
+from torchviz import make_dot,make_dot_from_trace
 import torch.nn.functional as F
 
 
@@ -17,11 +17,9 @@ class dw_conv(nn.Module):
             nn.Conv2d(in_dim, out_dim, kernel_size=3, stride=1, padding=1, groups=in_dim, bias=False),
             nn.BatchNorm2d(out_dim),
             activation())
-
     def forward(self, x):
         x = self.dw_conv_k3(x)
         return x
-
 
 # Initial Downsampling
 class InitialBlock(nn.Module):
@@ -55,17 +53,16 @@ class InitialBlock(nn.Module):
         self.out_activation = activation()
 
     def forward(self, x):
-        main = self.main_branch(x)  # 1 8 128 128
-        ext = self.ext_branch(x)  # 1 3 128 128
+        main = self.main_branch(x) #1 8 128 128
+        ext = self.ext_branch(x) #1 3 128 128
 
         # Concatenate branches
-        out = torch.cat((main, ext), 1)  # 1 16 128 128
+        out = torch.cat((main, ext), 1) # 1 16 128 128
 
         # Apply batch normalization
         out = self.batch_norm(out)
 
         return self.out_activation(out)
-
 
 # Regular/asymmetric/depthwise/dilated conv.
 class RegularBottleneck(nn.Module):
@@ -137,7 +134,7 @@ class RegularBottleneck(nn.Module):
                     stride=1,
                     bias=bias), nn.BatchNorm2d(internal_channels), activation())
 
-            self.ext_conv2 = dw_conv(internal_channels, internal_channels)  # 深度可分离卷积
+            self.ext_conv2 = dw_conv(internal_channels,internal_channels) #深度可分离卷积
 
             # 1x1 expansion convolution
             self.ext_conv3 = nn.Sequential(
@@ -228,7 +225,6 @@ class RegularBottleneck(nn.Module):
 
         return self.out_activation(out)
 
-
 # Middle Downsampling
 class DownsamplingBottleneck(nn.Module):
     def __init__(self,
@@ -239,6 +235,7 @@ class DownsamplingBottleneck(nn.Module):
                  bias=False,
                  relu=True):
         super().__init__()
+
 
         internal_channels = in_channels // internal_ratio
 
@@ -314,18 +311,10 @@ class DownsamplingBottleneck(nn.Module):
 
         return self.out_activation(out)
 
-
 # Lightweight Infrared small segmentation
-class LightWeightNetwork(nn.Module):
-    # def __init__(self, n_classes=1, encoder_relu=False, decoder_relu=True, channel=(8, 32, 64), dilations=(2,4,8,16), kernel_size=(3,5,7,9), padding=(1,2,3,4)):
-    # def __init__(self, n_classes=1, encoder_relu=False, decoder_relu=True, channel=(8, 32, 64), dilations=(2, 4, 8, 16),kernel_size=(7, 7, 7, 7), padding=(3, 3, 3, 3)):
-    def __init__(self, n_classes=1, encoder_relu=False, decoder_relu=True, channel=(8, 16, 32), dilations=(2, 4, 8, 16),kernel_size=(7, 7, 7, 7), padding=(3, 3, 3, 3)):
+class  LightWeightNetwork(nn.Module):
+    def __init__(self, n_classes=1, encoder_relu=False, decoder_relu=True, channel=(4, 16, 32), dilations=(2,4,8,16), kernel_size=(9,9,9,9), padding=(4,4,4,4)):
         super().__init__()
-
-        self.dropout_set1 = 0.01
-        self.dropout_set2 = 0.01
-        # self.dropout_set1 = 0
-        # self.dropout_set2 = 0
 
         # Stage 1 - Encoder
         self.initial_block = InitialBlock(3, channel[0], relu=encoder_relu)
@@ -334,90 +323,82 @@ class LightWeightNetwork(nn.Module):
         self.downsample1_0 = DownsamplingBottleneck(
             channel[0],
             channel[1],
-            dropout_prob=self.dropout_set1,
+            dropout_prob=0.01,
             relu=encoder_relu)
         self.regular1_1 = RegularBottleneck(
-            channel[1], padding=1, regular=True, dropout_prob=self.dropout_set1, relu=encoder_relu)
+            channel[1], padding=1, regular=True, dropout_prob=0.01, relu=encoder_relu)
         self.regular1_2 = RegularBottleneck(
-            channel[1], padding=1, regular=True, dropout_prob=self.dropout_set1, relu=encoder_relu)
+            channel[1], padding=1, regular=True, dropout_prob=0.01, relu=encoder_relu)
         self.regular1_3 = RegularBottleneck(
-            channel[1], padding=1, regular=True, dropout_prob=self.dropout_set1, relu=encoder_relu)
+            channel[1], padding=1, regular=True, dropout_prob=0.01, relu=encoder_relu)
         self.regular1_4 = RegularBottleneck(
-            channel[1], padding=1, regular=True, dropout_prob=self.dropout_set1, relu=encoder_relu)
+            channel[1], padding=1, regular=True, dropout_prob=0.01, relu=encoder_relu)
 
         # Stage 3 - Encoder
         self.downsample2_0 = DownsamplingBottleneck(
             channel[1],
             channel[2],
-            dropout_prob=self.dropout_set2,
+            dropout_prob=0.1,
             relu=encoder_relu)
-        # DAAA Module1
+        #DAAA Module1
         self.Depthwise2_1 = RegularBottleneck(
-            channel[2], padding=1, depthwise=True, dropout_prob=self.dropout_set2, relu=encoder_relu)
+            channel[2], padding=1, depthwise=True, dropout_prob=0.1, relu=encoder_relu)
         self.Atrous2_2 = RegularBottleneck(
-            channel[2], dilation=dilations[0], padding=dilations[0], dilated=True, dropout_prob=self.dropout_set2,
-            relu=encoder_relu)
+            channel[2], dilation=dilations[0], padding=dilations[0], dilated=True, dropout_prob=0.1, relu=encoder_relu)
         self.Asymmetric2_3 = RegularBottleneck(
             channel[2],
             kernel_size=kernel_size[0],
             padding=padding[0],
             asymmetric=True,
-            dropout_prob=self.dropout_set2,
+            dropout_prob=0.1,
             relu=encoder_relu)
         self.Atrous2_4 = RegularBottleneck(
-            channel[2], dilation=dilations[1], padding=dilations[1], dilated=True, dropout_prob=self.dropout_set2,
-            relu=encoder_relu)
+            channel[2], dilation=dilations[1], padding=dilations[1], dilated=True, dropout_prob=0.1, relu=encoder_relu)
 
         # DAAA Module2
         self.Depthwise2_5 = RegularBottleneck(
-            channel[2], padding=1, depthwise=True, dropout_prob=self.dropout_set2, relu=encoder_relu)
+            channel[2], padding=1, depthwise=True, dropout_prob=0.1, relu=encoder_relu)
         self.Atrous2_6 = RegularBottleneck(
-            channel[2], dilation=dilations[2], padding=dilations[2], dilated=True, dropout_prob=self.dropout_set2,
-            relu=encoder_relu)
+            channel[2], dilation=dilations[2], padding=dilations[2], dilated=True, dropout_prob=0.1, relu=encoder_relu)
         self.Asymmetric2_7 = RegularBottleneck(
             channel[2],
             kernel_size=kernel_size[1],
             padding=padding[1],
             asymmetric=True,
-            dropout_prob=self.dropout_set2,
+            dropout_prob=0.1,
             relu=encoder_relu)
         self.Atrous2_8 = RegularBottleneck(
-            channel[2], dilation=dilations[3], padding=dilations[3], dilated=True, dropout_prob=self.dropout_set2,
-            relu=encoder_relu)
+            channel[2], dilation=dilations[3], padding=dilations[3], dilated=True, dropout_prob=0.1, relu=encoder_relu)
 
         # DAAA Module3
         self.Depthwise3_1 = RegularBottleneck(
-            channel[2], padding=1, depthwise=True, dropout_prob=self.dropout_set2, relu=encoder_relu)
+            channel[2], padding=1, depthwise=True, dropout_prob=0.1, relu=encoder_relu)
         self.Atrous3_2 = RegularBottleneck(
-            channel[2], dilation=dilations[0], padding=dilations[0], dilated=True, dropout_prob=self.dropout_set2,
-            relu=encoder_relu)
+            channel[2], dilation=dilations[0], padding=dilations[0], dilated=True, dropout_prob=0.1, relu=encoder_relu)
         self.Asymmetric3_3 = RegularBottleneck(
             channel[2],
             kernel_size=kernel_size[2],
             padding=padding[2],
             asymmetric=True,
-            dropout_prob=self.dropout_set2,
+            dropout_prob=0.1,
             relu=encoder_relu)
         self.Atrous3_4 = RegularBottleneck(
-            channel[2], dilation=dilations[1], padding=dilations[1], dilated=True, dropout_prob=self.dropout_set2,
-            relu=encoder_relu)
+            channel[2], dilation=dilations[1], padding=dilations[1], dilated=True, dropout_prob=0.1, relu=encoder_relu)
 
         # DAAA Module4
         self.Depthwise3_5 = RegularBottleneck(
-            channel[2], padding=1, depthwise=True, dropout_prob=self.dropout_set2, relu=encoder_relu)
+            channel[2], padding=1, depthwise=True, dropout_prob=0.1, relu=encoder_relu)
         self.Atrous3_6 = RegularBottleneck(
-            channel[2], dilation=dilations[2], padding=dilations[2], dilated=True, dropout_prob=self.dropout_set2,
-            relu=encoder_relu)
+            channel[2], dilation=dilations[2], padding=dilations[2], dilated=True, dropout_prob=0.1, relu=encoder_relu)
         self.Asymmetric3_7 = RegularBottleneck(
             channel[2],
             kernel_size=kernel_size[3],
             padding=padding[3],
             asymmetric=True,
-            dropout_prob=self.dropout_set2,
+            dropout_prob=0.1,
             relu=encoder_relu)
         self.Atrous3_8 = RegularBottleneck(
-            channel[2], dilation=dilations[3], padding=dilations[3], dilated=True, dropout_prob=self.dropout_set2,
-            relu=encoder_relu)
+            channel[2], dilation=dilations[3], padding=dilations[3], dilated=True, dropout_prob=0.1, relu=encoder_relu)
 
         # Stage 4 - Decoder
         self.transposed4_conv = nn.ConvTranspose2d(
@@ -428,9 +409,9 @@ class LightWeightNetwork(nn.Module):
             padding=1,
             bias=False)
         self.regular4_1 = RegularBottleneck(
-            channel[1], padding=1, regular=True, dropout_prob=self.dropout_set2, relu=decoder_relu)
+            channel[1], padding=1, regular=True, dropout_prob=0.1, relu=decoder_relu)
         self.regular4_2 = RegularBottleneck(
-            channel[1], padding=1, regular=True, dropout_prob=self.dropout_set2, relu=decoder_relu)
+            channel[1], padding=1, regular=True, dropout_prob=0.1, relu=decoder_relu)
 
         # Stage 5 - Decoder
         self.transposed5_conv = nn.ConvTranspose2d(
@@ -441,7 +422,7 @@ class LightWeightNetwork(nn.Module):
             padding=1,
             bias=False)
         self.regular5_1 = RegularBottleneck(
-            channel[0], padding=1, regular=True, dropout_prob=self.dropout_set2, relu=decoder_relu)
+            channel[0], padding=1, regular=True, dropout_prob=0.1, relu=decoder_relu)
         # Stage 6 - Decoder
         self.transposed6_conv = nn.ConvTranspose2d(
             channel[0],
@@ -470,44 +451,45 @@ class LightWeightNetwork(nn.Module):
         self.conv2 = nn.Conv2d(32, 8, kernel_size=1, stride=1, padding=0, bias=False)
         self.conv3 = nn.Conv2d(8, n_classes, kernel_size=1, stride=1, padding=0, bias=False)
 
+
     def forward(self, x):
         # Stage 1-Encoder
-        input_size = x.size()  # 1 3 256 256
-        x1 = self.initial_block(x)  # 1 8 128 128
+        input_size = x.size()         # 1 3 256 256
+        x1 = self.initial_block(x)    # 1 8 128 128
 
         # Stage 2-Encoder
-        stage1_input_size = x1.size()  # 1 8 128 128
-        x2 = self.downsample1_0(x1)  # 1 32 64 64
-        x2 = self.regular1_1(x2)  # 1 32 64 64
-        x2 = self.regular1_2(x2)  # 1 32 64 64
-        x2 = self.regular1_3(x2)  # 1 32 64 64
-        x2 = self.regular1_4(x2)  # 1 32 64 64
+        stage1_input_size = x1.size() # 1 8 128 128
+        x2 = self.downsample1_0(x1)   # 1 32 64 64
+        x2 = self.regular1_1(x2)      # 1 32 64 64
+        x2 = self.regular1_2(x2)      # 1 32 64 64
+        x2 = self.regular1_3(x2)      # 1 32 64 64
+        x2 = self.regular1_4(x2)      # 1 32 64 64
 
         # Stage3.1 -Encoder
-        stage2_input_size = x2.size()  # 1 32 64 64
-        x3 = self.downsample2_0(x2)  # 1 64 32 32
-        # DAAA Module1
-        x3 = self.Depthwise2_1(x3)  # 1 64 32 32
-        x3 = self.Atrous2_2(x3)  # 1 64 32 32
-        x3 = self.Asymmetric2_3(x3)  # 1 64 32 32
-        x3 = self.Atrous2_4(x3)  # 1 64 32 32
-        # DAAA Module2
-        x3 = self.Depthwise2_5(x3)  # 1 64 32 32
-        x3 = self.Atrous2_6(x3)  # 1 64 32 32
-        x3 = self.Asymmetric2_7(x3)  # 1 64 32 32
-        x3 = self.Atrous2_8(x3)  # 1 64 32 32
+        stage2_input_size = x2.size() # 1 32 64 64
+        x3 = self.downsample2_0(x2)   # 1 64 32 32
+        #DAAA Module1
+        x3 = self.Depthwise2_1(x3)    # 1 64 32 32
+        x3 = self.Atrous2_2(x3)       # 1 64 32 32
+        x3 = self.Asymmetric2_3(x3)   # 1 64 32 32
+        x3 = self.Atrous2_4(x3)       # 1 64 32 32
+        #DAAA Module2
+        x3 = self.Depthwise2_5(x3)    # 1 64 32 32
+        x3 = self.Atrous2_6(x3)       # 1 64 32 32
+        x3 = self.Asymmetric2_7(x3)   # 1 64 32 32
+        x3 = self.Atrous2_8(x3)       # 1 64 32 32
 
         # Stage3.2 -Encoder
-        # DAAA Module3
-        x3 = self.Depthwise3_1(x3)  # 1 64 32 32
-        x3 = self.Atrous3_2(x3)  # 1 64 32 32
-        x3 = self.Asymmetric3_3(x3)  # 1 64 32 32
-        x3 = self.Atrous3_4(x3)  # 1 64 32 32
-        # DAAA Module4
-        x3 = self.Depthwise3_5(x3)  # 1 64 32 32
-        x3 = self.Atrous3_6(x3)  # 1 64 32 32
-        x3 = self.Asymmetric3_7(x3)  # 1 64 32 32
-        x3 = self.Atrous3_8(x3)  # 1 64 32 32
+        #DAAA Module3
+        x3 = self.Depthwise3_1(x3)    # 1 64 32 32
+        x3 = self.Atrous3_2(x3)       # 1 64 32 32
+        x3 = self.Asymmetric3_3(x3)   # 1 64 32 32
+        x3 = self.Atrous3_4(x3)       # 1 64 32 32
+        #DAAA Module4
+        x3 = self.Depthwise3_5(x3)    # 1 64 32 32
+        x3 = self.Atrous3_6(x3)       # 1 64 32 32
+        x3 = self.Asymmetric3_7(x3)   # 1 64 32 32
+        x3 = self.Atrous3_8(x3)       # 1 64 32 32
 
         # Stage4 -Decoder
         x4 = self.transposed4_conv(x3, output_size=stage2_input_size)  # 1 32 64 64
@@ -515,12 +497,12 @@ class LightWeightNetwork(nn.Module):
         # x4 = self.conv1(x4)
 
         # sum
-        x4 = x4 + x2  # 1 32 64 64
+        x4 = x4 + x2                                                   # 1 32 64 64
         # concat
         # x4 = torch.cat([x4, x2], dim=1)
         # x4 = self.ext_conv1(x4)
-        x4 = self.regular4_1(x4)  # 1 32 64 64
-        x4 = self.regular4_2(x4)  # 1 32 64 64
+        x4 = self.regular4_1(x4)                                       # 1 32 64 64
+        x4 = self.regular4_2(x4)                                       # 1 32 64 64
 
         # Stage5 -Decoder
         x5 = self.transposed5_conv(x4, output_size=stage1_input_size)  # 1 8 128 128
@@ -528,37 +510,37 @@ class LightWeightNetwork(nn.Module):
         # x5 = self.conv2(x5)
 
         # sum
-        x5 = x5 + x1  # 1 8 128 128
+        x5 = x5 + x1                                                   # 1 8 128 128
         # concat
         # x5 = torch.cat([x5, x1], dim=1)
         # x5 = self.ext_conv2(x5)
-        x5 = self.regular5_1(x5)  # 1 8 128 128
+        x5 = self.regular5_1(x5)                                       # 1 8 128 128
 
         # Stage6 -Decoder
-        x6 = self.transposed6_conv(x5, output_size=input_size)  # 1 1  256 256
+        x6 = self.transposed6_conv(x5, output_size=input_size)         # 1 1  256 256
         # x6 = self.conv3(x5)  # 1 16 128 128  1*1conv.
         # x6 = F.interpolate(x6, size=(256, 256), mode='bilinear', align_corners=True)
 
-        return x6
 
+        return x6
 
 if __name__ == '__main__':
     from thop import profile
     import time
     import matplotlib.pyplot as plt
-
-    inputs = torch.randn((1, 3, 256, 256))
+    inputs = torch.randn((1, 3, 512, 512))
     start = time.perf_counter()
-    model = LWNet(channel=(8, 32, 64), dilations=(2, 4, 8, 16), kernel_size=(7, 7, 7, 7),
-                  padding=(3, 3, 3, 3))  # kernel_size/padding = 5/2 7/3 9/4
-    # 可视化
+    #model = LW_IRST_ablation(channel=(8, 32, 64), dilations=(2,4,8,16), kernel_size=(7,7,7,7), padding=(3,3,3,3)) # kernel_size/padding = 5/2 7/3 9/4
+    model = LightWeightNetwork()
+    #可视化
     # graph = make_dot(model(inputs), params=dict(model.named_parameters()))
+
+
     out = model(inputs)
     end = time.perf_counter()
     FLOPs, params = profile(model, inputs=(inputs,))
     running_FPS = 1 / (end - start)
     print('running_FPS:', running_FPS)
-    print('FLOPs=', str(FLOPs / 1000000.0) + '{}'.format('M'))
+    print('FLOPs=', str(FLOPs/1000000.0) + '{}'.format('M'))
     print('params=', str(params / 1000000.0) + '{}'.format('M'))
     print(out.size())
-    pass
