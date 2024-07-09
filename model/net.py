0682baa5adf837cc021863f8c947a6857f27d690
SwordShiSan <2486511577@qq.com>
2024-07-09 10:23:01 +0800

    lw half 1024

--- model/net.py
+++ model/net.py
@@ -311,7 +311,7 @@ class DownsamplingBottleneck(nn.Module):
 
 # Lightweight Infrared small segmentation
 class  LightWeightNetwork(nn.Module):
-    def __init__(self, n_classes=1, encoder_relu=False, decoder_relu=True, channel=(8, 16, 32), dilations=(2,4,8,16), kernel_size=(7,7,7,7), padding=(3,3,3,3)):
+    def __init__(self, n_classes=1, encoder_relu=False, decoder_relu=True, channel=(4, 16, 32), dilations=(2,4,8,16), kernel_size=(7,7,7,7), padding=(3,3,3,3)):
         super().__init__()
 
         # Stage 1 - Encoder
